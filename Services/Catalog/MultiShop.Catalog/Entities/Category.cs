448b8355fe75e9ed0a9a043c4e6bfae9659942f0
bunyamin <bnymtmt17@gmail.com>
2025-03-12 03:03:55 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId] 
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)] //Bunun benzersiz olduğunu belirtmek için kullanılır
+        public string CategoryID { get; set; }
+
+        public  string CategoryName { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductImage
+    {
+        public string ProductImagesId { get; set; }
+        public string Images1 { get; set; }
+        public string Images2 { get; set; }
+        public string Images3 { get; set; }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+
+    }
+}
