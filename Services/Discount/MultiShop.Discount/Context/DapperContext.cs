ecbf4f3046d0d25a9dd9356e9ebf3485c7cb8df1
bunyamin <bnymtmt17@gmail.com>
2025-04-30 02:00:20 +0300

    begin the discount service

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString= _configuration.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-7PLSN8S; initial Catalog=MultiShopDiscountDb; integrated Security=true;TrustServerCertificate=True");
+        }
+        public DbSet<MultiShop.Discount.Entities.Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
