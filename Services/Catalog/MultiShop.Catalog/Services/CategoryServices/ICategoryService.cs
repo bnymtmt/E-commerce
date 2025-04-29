055da9c007d47ad3d6693c44b11e482321e867ee
bunyamin <bnymtmt17@gmail.com>
2025-04-29 18:09:46 +0300

    category and product classes

--- Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -4,7 +4,7 @@ namespace MultiShop.Catalog.Services.CategoryServices
 {
     public interface ICategoryService
     {
-        Task<List<ResultCategoryDto>> GettAllCategoryAsync();
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
         Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
         Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
         Task DeleteCategoryAsync(string id);
