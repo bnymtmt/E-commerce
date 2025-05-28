9ba695645b7d28e4c6b3bd18376bd5a3715ed5b0
bunyamin <bnymtmt17@gmail.com>
2025-05-28 23:58:46 +0300

    frontends begin

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/FeatureSliderController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureSliderDtos;
+using MultiShop.WebUI.Services.CatalogServices.FeatureSliderServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/FeatureSlider")]
+    public class FeatureSliderController : Controller
+    {
+        private readonly IFeatureSliderService _featureSliderService;
+
+        public FeatureSliderController(IFeatureSliderService featureSliderService)
+        {
+            _featureSliderService = featureSliderService;
+        }
+
+        void FeatureSliderViewBagList()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Öne Çıkan Görseller";
+            ViewBag.v3 = "Slider Öne Çıkan Listesi";
+            ViewBag.v0 = "Slider Öne Çıkan Görsel İşlemleri";
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            FeatureSliderViewBagList();
+            var values = await _featureSliderService.GetAllFeatureSliderAsync();
+            return View(values);
+        }
+
+        [HttpGet]
+        [Route("CreateFeatureSlider")]
+        public IActionResult CreateFeatureSlider()
+        {
+            FeatureSliderViewBagList();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateFeatureSlider")]
+        public async Task<IActionResult> CreateFeatureSlider(CreateFeatureSliderDto createFeatureSliderDto)
+        {
+            await _featureSliderService.CreateFeatureSliderAsync(createFeatureSliderDto);
+            return RedirectToAction("Index", "FeatureSlider", new { area = "Admin" });
+        }
+
+        [Route("DeleteFeatureSlider/{id}")]
+        public async Task<IActionResult> DeleteFeatureSlider(string id)
+        {
+            await _featureSliderService.DeleteFeatureSliderAsync(id);
+            return RedirectToAction("Index", "FeatureSlider", new { area = "Admin" });
+        }
+
+        [HttpGet]
+        [Route("UpdateFeatureSlider/{id}")]
+        public async Task<IActionResult> UpdateFeatureSlider(string id)
+        {
+            FeatureSliderViewBagList();
+            var value = await _featureSliderService.GetByIdFeatureSliderAsync(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        [Route("UpdateFeatureSlider/{id}")]
+        public async Task<IActionResult> UpdateFeatureSlider(UpdateFeatureSliderDto updateFeatureSliderDto)
+        {
+            await _featureSliderService.UpdateFeatureSliderAsync(updateFeatureSliderDto);
+            return RedirectToAction("Index", "FeatureSlider", new { area = "Admin" });
+        }
+    }
+}
